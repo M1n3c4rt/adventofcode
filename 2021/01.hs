f51fbc1f95818eecf237a134bbe93d58d7e4e0fd
M1n3c4rt <vedicbits@gmail.com>
2025-03-01 17:03:58 +0530

    add 2021 days 1 to 5

---
+++ 2021/01.hs
@@ -0,0 +1,19 @@
+
+main :: IO ()
+main = do
+    contents <- readFile "01.txt"
+    -- part 1
+    print $ length $ filter (>0) $ diffList $ parse contents
+    -- part 2
+    print $ length $ filter (>0) $ diffList $ sumList $ parse contents
+
+parse :: String -> [Int]
+parse = map read . lines
+
+diffList :: [Int] -> [Int]
+diffList (x:y:xs) = (y-x):diffList (y:xs)
+diffList [x] = []
+
+sumList :: [Int] -> [Int]
+sumList (a:b:c:xs) = (a+b+c):sumList (b:c:xs)
+sumList [a,b] = []
---
+++ 2021/03.hs
@@ -0,0 +1,41 @@
+import Data.List (transpose, sort, group, maximumBy, minimumBy, sortBy)
+import Data.Function (on)
+import Data.Ord (comparing, Down (Down))
+
+main :: IO ()
+main = do
+    contents <- readFile "03.txt"
+    let p = parse contents
+    let p' = transpose p
+    -- part 1
+    print $ binToDec (map mode p') * binToDec (map unmode p')
+    -- part 2
+    print $ binToDec (filterCO2 p 0) * binToDec (filterOxygen p 0)
+
+parse :: String -> [[Int]]
+parse = map (map (read . return)) . lines
+
+mode :: Ord a => [a] -> a
+mode = head . maximumBy (compare `on` length) . group . sort
+
+unmode :: Ord a => [a] -> a
+unmode = head . minimumBy (compare `on` length) . group . sort
+
+binToDec :: [Int] -> Int
+binToDec = sum . zipWith (*) (map (2^) [0..]) . reverse
+
+filterOxygen :: Ord a => [[a]] -> Int -> [a]
+filterOxygen ns i =
+    let m = mode (map (!!i) ns)
+        new = filter ((==m) . (!!i)) ns
+    in case new of
+        [a] -> a
+        as -> filterOxygen as (i+1)
+
+filterCO2 :: Ord a => [[a]] -> Int -> [a]
+filterCO2 ns i =
+    let m = unmode (map (!!i) ns)
+        new = filter ((==m) . (!!i)) ns
+    in case new of
+        [a] -> a
+        as -> filterCO2 as (i+1)
